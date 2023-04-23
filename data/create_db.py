b86216fccd75a5cdcd396d3105abf13000d60a1a
a-menshikov <a.menshikov1989@gmail.com>
2023-04-23 17:30:10 +0300

    Update create_db.py

--- data/create_db.py
+++ data/create_db.py
@@ -1,6 +1,8 @@
 from data.db_loader import Base, engine
+from data.services import fill_notifications
 
 
 async def create_db():
     """Создание БД и наполнение ее предустановленными данными."""
     Base.metadata.create_all(engine)
+    fill_notifications()
