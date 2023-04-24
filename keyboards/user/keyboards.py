abd76f3b48209d3ddf707d9b0948e679e4dbb414
a-menshikov <a.menshikov1989@gmail.com>
2023-04-24 11:41:50 +0300

    дополнена клавиатура главного меню, создана клавиатура настройки подписок

--- keyboards/user/keyboards.py
+++ keyboards/user/keyboards.py
@@ -2,6 +2,9 @@ from aiogram.types import KeyboardButton, ReplyKeyboardMarkup
 
 reg_button = "Регистрация"
 calendar = "Календарь игр"
+notification = "Настройка уведомлений"
+yes_notification = "Уведомлять"
+no_notification = "Не уведомлять"
 admin_menu = "Меню админа"
 cancel_button = "Отмена"
 in_main_menu = "В главное меню"
@@ -14,11 +17,23 @@ def reg_keyboard():
     return markup
 
 
+def notification_keyboard(check: bool):
+    """Клавиатура настройки уведомлений."""
+    markup = ReplyKeyboardMarkup(resize_keyboard=True, selective=True)
+    if check:
+        markup.add(KeyboardButton(no_notification))
+    else:
+        markup.add(KeyboardButton(yes_notification))
+    markup.add(KeyboardButton(in_main_menu))
+    return markup
+
+
 def menu_reply_keyboard(admin: bool = False):
     """Клавиатура главного меню юзера."""
     markup = ReplyKeyboardMarkup(resize_keyboard=True, selective=True)
     markup.row(
         KeyboardButton(calendar),
+        KeyboardButton(notification),
     )
     if admin:
         markup.row(
