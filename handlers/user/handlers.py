d8890258d2cfdcf450f9935012eee31afd88946f
a-menshikov <a.menshikov1989@gmail.com>
2023-04-24 20:38:36 +0300

    хэндлер переключатель статуса уведомлений

--- handlers/user/handlers.py
+++ handlers/user/handlers.py
@@ -3,11 +3,12 @@ from aiogram.dispatcher import Dispatcher
 from data.services import (create_new_notification, create_new_user,
                            get_calendar, get_user_notification_status,
                            is_admin, is_user_exist_in_base,
-                           make_user_calendar_message)
+                           make_user_calendar_message, notification_switcher)
 from keyboards.user.keyboards import (calendar, in_main_menu,
-                                      menu_reply_keyboard, notification,
-                                      notification_keyboard, reg_button,
-                                      reg_keyboard)
+                                      menu_reply_keyboard, no_notification,
+                                      notification, notification_keyboard,
+                                      reg_button, reg_keyboard,
+                                      yes_notification)
 from loader import logger
 
 
@@ -91,9 +92,28 @@ async def notification_menu(message: types.Message):
                              reply_markup=notification_keyboard(check))
 
 
+async def notification_switch(message: types.Message):
+    """Переключение статуса рассылки уведомлений."""
+    telegram_id = message.from_user.id
+    admin = is_admin(telegram_id)
+    switch_status = notification_switcher(telegram_id)
+    if not switch_status:
+        none_message = ("Что-то пошло не так. Попробуйте отправить боту "
+                        "/start и убедитесь, что вы зарегистрированы. Если "
+                        "не получится - обращайтесь к @Menshikov_AS")
+        await message.answer(none_message,
+                             reply_markup=menu_reply_keyboard(admin))
+    else:
+        yes_message = "Статус подписки на уведомления успешно изменён."
+        await message.answer(yes_message,
+                             reply_markup=menu_reply_keyboard(admin))
+
+
 def register_user_handlers(dp: Dispatcher):
     dp.register_message_handler(starter, commands=["start", "help"])
     dp.register_message_handler(main_menu, text=in_main_menu)
     dp.register_message_handler(registration, text=reg_button)
     dp.register_message_handler(get_future_calendar, text=calendar)
     dp.register_message_handler(notification_menu, text=notification)
+    dp.register_message_handler(notification_switch,
+                                text=(no_notification, yes_notification))
