c79d5fcbdeae566e93e3971c3e84734c03b10ff9
a-menshikov <a.menshikov1989@gmail.com>
2023-04-25 11:45:27 +0300

    Добавлено задание на ежедневную рассылку уведомлений

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
+import asyncio
+
+import aioschedule
 from aiogram.utils import executor
 
 from data.create_db import create_db
+from data.services import notificate
 from handlers.admin.add_new_event import register_add_event_handlers
 from handlers.admin.delete_event import register_delete_event_handlers
 from handlers.admin.edit_event import register_edit_event_handlers
@@ -12,11 +16,23 @@ from loader import TECH, bot, dp, logger
 async def on_startup(_):
     """Выполняется при старте бота."""
     await create_db()
+    loop = asyncio.get_event_loop()
+    loop.create_task(scheduler())
     try:
         await bot.send_message(TECH, 'Бот запущен')
     except Exception:
         pass
     logger.info('Бот запущен')
+
+
+async def scheduler():
+    """Расписание выполнения задач."""
+    scheduler = aioschedule.Scheduler()
+    scheduler.every().day.at("11:45").do(notificate)
+    while True:
+        await scheduler.run_pending()
+        await asyncio.sleep(1)
+
 
 register_user_handlers(dp)
 register_admin_handlers(dp)
